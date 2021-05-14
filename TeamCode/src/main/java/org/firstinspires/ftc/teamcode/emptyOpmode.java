86b7178f02b8174b574b9ed25eafd82402c18711
LauraAEvans <elzagirl10@gmail.com>
2021-05-13 20:51:18 -0500

    emptied basic empty opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/emptyOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/emptyOpmode.java
@@ -20,10 +20,6 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 public class emptyOpmode extends LinearOpMode
 {
-public DcMotor motorRF = null;
-public DcMotor motorRB = null;
-public DcMotor motorLF = null;
-public DcMotor motorLB = null;
 
     @Override
     public void runOpMode() throws InterruptedException
@@ -35,22 +31,11 @@ public DcMotor motorLB = null;
 
         ElapsedTime whatever = new ElapsedTime();
 
-        motorRF = hardwareMap.dcMotor.get("motorRF");
-        motorRB = hardwareMap.dcMotor.get("motorRB");
-        motorLF = hardwareMap.dcMotor.get("motorLF");
-        motorLB = hardwareMap.dcMotor.get("motorLB");
-
-        motorLF.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorLB.setDirection(DcMotorSimple.Direction.REVERSE);
-
         waitForStart();
 
         while (opModeIsActive())
         {
-            motorRF.setPower( ((- - - - -gamepad1.right_stick_y + - - - gamepad1.right_stick_x)) + - - - (gamepad1.left_stick_x) );
-            motorRB.setPower( ((- - - - -gamepad1.right_stick_y - - - - gamepad1.right_stick_x)) + - - - (gamepad1.left_stick_x) );
-            motorLB.setPower( ((- - - - -gamepad1.right_stick_y - - - gamepad1.right_stick_x)) - - - - (gamepad1.left_stick_x) );
-            motorLF.setPower( ((- - - - -gamepad1.right_stick_y + - - gamepad1.right_stick_x)) - - - -(gamepad1.left_stick_x) );
+
         }
     }
 }
